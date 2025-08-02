1d93fc8dadf9b264b60247c0c341165c63005c94
neoskyclad <neoskyclad@naver.com>
2025-08-02 12:04:34 +0900

    feat: 구글 소셜 로그인 로직 추가

--- src/main/java/com/moup/server/controller/AuthController.java
+++ src/main/java/com/moup/server/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.moup.server.controller;
 
-import com.moup.server.exception.DuplicateUserException;
-import com.moup.server.exception.ErrorCode;
-import com.moup.server.exception.UserNotFoundException;
+import com.moup.server.common.Login;
 import com.moup.server.model.dto.LoginRequest;
 import com.moup.server.model.dto.RegisterRequest;
 import com.moup.server.model.entity.User;
 import com.moup.server.service.AuthService;
+import com.moup.server.service.AuthServiceFactory;
+import com.moup.server.service.GoogleAuthService;
 import com.moup.server.service.UserService;
 import com.moup.server.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,11 +15,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Arrays;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +40,7 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceFactory authServiceFactory;
     private final UserService userService;
     private final JwtUtil jwtUtil;
 
@@ -61,17 +60,30 @@ public class AuthController {
             )
     )
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        Login provider = Login.valueOf(loginRequest.getProvider());
         String idToken = loginRequest.getIdToken();
-        // 토큰 유효성 검증
-        if (!authService.validateToken(idToken)) {
-            
+
+        String providerId = "";
+
+        try {
+            AuthService service = authServiceFactory.getService(provider);
+
+            Map<String, Object> userInfo = service.verifyIdToken(idToken);
+            providerId = userInfo.get("userId").toString();
+
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
         }
-        
+
         // 토큰 파싱 후 사용자 확인
-//        User user = userService.findByProviderId(loginRequest.getProviderId());
-        
+        User user = userService.findByProviderAndId(provider, providerId);
+
         // 1. 없으면 회원가입 -> 자동으로
-        
+        if (user == null) {
+
+        } else {
+
+        }
         // 2. 있으면 그대로 로그인
 
         // Access Token 반환
--- src/main/java/com/moup/server/service/AuthService.java
+++ src/main/java/com/moup/server/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.moup.server.service;
 
+import com.moup.server.common.Login;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Map;
+
 public interface AuthService {
-    boolean validateToken(String token);
+    Login getProvider();
+    Map<String, Object> verifyIdToken(String idToken) throws GeneralSecurityException, IOException;
 }
