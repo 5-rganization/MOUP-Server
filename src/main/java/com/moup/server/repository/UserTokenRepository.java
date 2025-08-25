7a7de7760f92afa692ba69730cff7080ecf3994c
neoskyclad <neoskyclad@naver.com>
2025-08-26 01:57:54 +0900

    fix: UserTokenRepository 오타 수정

--- src/main/java/com/moup/server/repository/UserTokenRepository.java
+++ src/main/java/com/moup/server/repository/UserTokenRepository.java
@@ -16,7 +16,7 @@ public interface UserTokenRepository {
     @Select("SELECT * FROM user_tokens WHERE user_id = #{userId}")
     Optional<UserToken> findByUserId(Long userId);
 
-    @Update("UPDATE user_tokens SET refresh_token = #{refreshToken}, expiry_date = #{expiryDate}, create_at = CURRENT_TIMESTAMP() WHERE id = #{id}")
+    @Update("UPDATE user_tokens SET refresh_token = #{refreshToken}, expiry_date = #{expiryDate}, created_at = CURRENT_TIMESTAMP() WHERE id = #{id}")
     void updateById(Long id, String refreshToken, String expiryDate);
 
     @Insert("INSERT INTO user_tokens (user_id, refresh_token, expiry_date) VALUES (#{userId}, #{refreshToken}, #{expiryDate})")
