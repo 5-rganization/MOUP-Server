cc477ca899fe23289ea53857eed1a0286d892b62
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-09-14 17:19:39 +0900

    feat: 조회하는 근무지가 존재하지 않을 때 발생하는 예외 구현
    GitHub Issue: #41

--- src/main/java/com/moup/server/exception/ErrorCode.java
+++ src/main/java/com/moup/server/exception/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_401", "인증되지 않은 사용자입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "해당 유저는 존재하지 않습니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER_409", "이미 등록된 유저입니다."),
+    WORKPLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "WORKPLACE_404", "해당 근무지는 존재하지 않습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버에 오류가 발생했습니다."),
     ALREADY_DELETED(HttpStatus.CONFLICT, "DELETE_409", "삭제 처리 중입니다.");
 
---
+++ src/main/java/com/moup/server/exception/WorkplaceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.moup.server.exception;
+
+public class WorkplaceNotFoundException extends CustomException {
+
+    @Override
+    public ErrorCode getErrorCode() {
+        return ErrorCode.WORKPLACE_NOT_FOUND;
+    }
+}
