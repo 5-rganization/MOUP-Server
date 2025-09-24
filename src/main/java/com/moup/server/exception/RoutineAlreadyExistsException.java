ee817e0abb2d781b527967461dbbc96cf556e10d
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-09-24 16:29:01 +0900

    fix: 불필요한 데이터 전달 코드 제거 및 루틴 서비스 로직 최적화
    GitHut Issue: #43

--- src/main/java/com/moup/server/exception/RoutineAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.moup.server.exception;
-
-public class RoutineAlreadyExistsException extends CustomException {
-    public ErrorCode getErrorCode() {
-        return ErrorCode.ROUTINE_ALREADY_EXISTS;
-    }
-}
