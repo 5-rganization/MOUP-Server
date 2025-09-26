1545e7b7739b18dfbf1a851b21898482e840a78e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-09-26 18:54:21 +0900

    feat: 필드 형식 검증 코드 추가
    GitHub Issue: #43

--- src/main/java/com/moup/server/exception/InvalidDateTimeFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.moup.server.exception;
-
-public class InvalidDateTimeFormatException extends CustomException {
-    @Override
-    public ErrorCode getErrorCode() {
-        return ErrorCode.INVALID_DATETIME_FORMAT;
-    }
-}
--- src/main/java/com/moup/server/model/dto/RoutineUpdateRequest.java
+++ src/main/java/com/moup/server/model/dto/RoutineUpdateRequest.java
@@ -1,8 +1,12 @@
 package com.moup.server.model.dto;
 
-import com.moup.server.exception.InvalidDateTimeFormatException;
+import com.moup.server.exception.InvalidFieldFormatException;
 import com.moup.server.model.entity.Routine;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -17,12 +21,17 @@ import java.util.List;
 public class RoutineUpdateRequest {
     static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
+    @NotNull(message = "필수 입력값입니다.")
     @Schema(description = "루틴 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long routineId;
+    @NotBlank(message = "빈 값 혹은 공백 문자는 받을 수 없습니다.")
     @Schema(description = "루틴 이름", example = "오픈", requiredMode = Schema.RequiredMode.REQUIRED)
     private String routineName;
-    @Schema(description = "알림 시간 (HH:mm)", example = "08:00", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Pattern(regexp = "^([01][0-9]|2[0-3]):[0-5][0-9]$", message = "알람 시간은 HH:mm 형식(예: 14:30)으로 입력해주세요.")
+    @Schema(description = "알림 시간 (HH:mm)", example = "14:30", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String alarmTime;
+    @NotNull(message = "값이 없을 경우 빈 배열을 전달해야 합니다.")
+    @Valid
     @Schema(description = "할 일 리스트",
             example = """
             [
@@ -62,7 +71,7 @@ public class RoutineUpdateRequest {
                         .alarmTime(LocalTime.parse(alarmTime, formatter))
                         .build();
             } catch (DateTimeParseException e) {
-                throw new InvalidDateTimeFormatException();
+                throw new InvalidFieldFormatException();
             }
         }
     }
