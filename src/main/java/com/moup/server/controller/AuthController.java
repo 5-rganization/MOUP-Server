1fb6f79b8fe83dcd9f262d95d5460843a5aa5633
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-10-11 01:36:52 +0900

    fix: Swagger의 예시 응답에서 userId 필드 제거
    GitHub Issue: #62

--- src/main/java/com/moup/server/controller/AuthController.java
+++ src/main/java/com/moup/server/controller/AuthController.java
@@ -54,7 +54,6 @@ public class AuthController {
             @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class, example =
                     """
                     {
-                        "userId": 1,
                         "role": "ROLE_WORKER",
                         "accessToken": "string",
                         "refreshToken": "string"
@@ -63,7 +62,6 @@ public class AuthController {
             @ApiResponse(responseCode = "201", description = "회원가입 절차 시작 (유저 생성)", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class, example =
                     """
                     {
-                        "userId": 1,
                         "accessToken": "string",
                         "refreshToken": "string"
                     }
@@ -71,7 +69,6 @@ public class AuthController {
             @ApiResponse(responseCode = "202", description = "회원가입 절차 진행중", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class, example =
                     """
                     {
-                        "userId": 1,
                         "accessToken": "string",
                         "refreshToken": "string"
                     }
