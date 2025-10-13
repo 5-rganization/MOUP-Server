feb111cc04be7cfea13413d0e8445d68e4217150
neoskyclad <neoskyclad@naver.com>
2025-10-13 11:35:44 +0900

    merge: main 브랜치 머지 및 Repository 어노테이션 수정

--- src/main/java/com/moup/server/repository/FCMTokenRepository.java
+++ src/main/java/com/moup/server/repository/FCMTokenRepository.java
@@ -1,9 +1,10 @@
 package com.moup.server.repository;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Mapper
 public interface FCMTokenRepository {
 
   @Update("UPDATE users SET fcm_token = #{fcmToken} WHERE id = #{userId}")
