48b146e1ab76bcfb82059cac1e91de652272f1c2
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-10-13 17:10:35 +0900

    feat: 관리자 컨트롤러에 삭제 상태 유저 즉시 삭제 API 추가
    GitHub Issue: #70

--- src/main/java/com/moup/server/controller/AdminController.java
+++ src/main/java/com/moup/server/controller/AdminController.java
@@ -21,13 +21,26 @@ public class AdminController {
     @DeleteMapping("/users")
     @Operation(summary = "유저 DB 영구 삭제", description = "유예 기간 이상 삭제 상태인 유저를 DB에서 영구 삭제")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "삭제 성공"),
+            @ApiResponse(responseCode = "204", description = "삭제 성공"),
             @ApiResponse(responseCode = "401", description = "인증 실패"),
             @ApiResponse(responseCode = "403", description = "권한 없음")
     })
     @PreAuthorize("hasRole('ROLE_ADMIN')") // 관리자 권한 필요
     public ResponseEntity<Void> hardDeleteOldUsers() {
         adminService.hardDeleteOldUsers();
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/users/immediate")
+    @Operation(summary = "모든 삭제 상태 유저 즉시 영구 삭제", description = "유예 기간에 상관없이 모든 삭제 상태의 유저를 DB에서 즉시 영구 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "삭제 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
+            @ApiResponse(responseCode = "403", description = "권한 없음")
+    })
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> hardDeleteUsersImmediately() {
+        adminService.hardDeleteUsersImmediately();
+        return ResponseEntity.noContent().build();
     }
 }
