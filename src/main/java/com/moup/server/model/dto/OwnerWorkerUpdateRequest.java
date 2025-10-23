be5ee0fabcc3c4f4f8f1c8f897a8e7b0741354f6
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-10-23 23:59:41 +0900

    chore: 미사용 코드 제거
    GitHub Issue: #82

--- src/main/java/com/moup/server/model/dto/OwnerWorkerUpdateRequest.java
+++ src/main/java/com/moup/server/model/dto/OwnerWorkerUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.moup.server.model.dto;
 
-import com.moup.server.model.entity.Worker;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -15,16 +14,4 @@ public class OwnerWorkerUpdateRequest extends BaseWorkerUpdateRequest {
     @NotBlank(message = "빈 값이나 공백 문자는 받을 수 없습니다.")
     @Schema(description = "라벨 색상 (사장님 기준)", example = "BLUE", requiredMode = Schema.RequiredMode.REQUIRED)
     private String ownerBasedLabelColor;
-
-    public Worker toEntity(Long workerId, Long userId, Long workplaceId, String workerBasedLabelColor) {
-        return Worker.builder()
-                .id(workerId)
-                .userId(userId)
-                .workplaceId(workplaceId)
-                .workerBasedLabelColor(workerBasedLabelColor)
-                .ownerBasedLabelColor(ownerBasedLabelColor)
-                .isAccepted(true)
-                .isNowWorking(false)
-                .build();
-    }
 }
--- src/main/java/com/moup/server/model/dto/WorkerWorkerUpdateRequest.java
+++ src/main/java/com/moup/server/model/dto/WorkerWorkerUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.moup.server.model.dto;
 
-import com.moup.server.model.entity.Worker;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -15,16 +14,4 @@ public class WorkerWorkerUpdateRequest extends BaseWorkerUpdateRequest {
     @NotBlank(message = "빈 값이나 공백 문자는 받을 수 없습니다.")
     @Schema(description = "라벨 색상 (알바생 기준)", example = "RED", requiredMode = Schema.RequiredMode.REQUIRED)
     private String workerBasedLabelColor;
-
-    public Worker toEntity(Long workerId, Long userId, Long workplaceId, String ownerBasedLabelColor) {
-        return Worker.builder()
-                .id(workerId)
-                .userId(userId)
-                .workplaceId(workplaceId)
-                .workerBasedLabelColor(workerBasedLabelColor)
-                .ownerBasedLabelColor(ownerBasedLabelColor)
-                .isAccepted(true)
-                .isNowWorking(false)
-                .build();
-    }
 }
