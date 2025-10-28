a1dc354a9043f1f0974dc2105ebe927046cc7ffd
neoskyclad <neoskyclad@naver.com>
2025-10-28 21:55:01 +0900

    fix: Docker server 연동 작업

--- server/src/main/java/com/moup/server/config/FCMConfig.java
+++ server/src/main/java/com/moup/server/config/FCMConfig.java
@@ -3,33 +3,41 @@ package com.moup.server.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
+@RequiredArgsConstructor
 public class FCMConfig {
 
-    @Value("${firebase.account-key.path}")
-    private String fireBaseKeyPath;
+  private final ResourceLoader resourceLoader;
+  @Value("${firebase.account-key.path}")
+  private String fireBaseKeyPath;
 
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
+  @Bean
+  public FirebaseApp initializeFirebase() throws IOException {
 
-        FileInputStream serviceAccount =
-                new FileInputStream(fireBaseKeyPath);
+    Resource resource = resourceLoader.getResource(fireBaseKeyPath);
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+    try (InputStream serviceAccount = resource.getInputStream()) {
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          // .setDatabaseUrl(...) // 필요하다면 추가
+          .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            return FirebaseApp.initializeApp(options);
-        } else {
-            return FirebaseApp.getInstance();
-        }
+      if (FirebaseApp.getApps().isEmpty()) { // 앱이 이미 초기화되지 않았는지 확인
+        return FirebaseApp.initializeApp(options);
+      } else {
+        return FirebaseApp.getInstance(); // 이미 초기화된 경우 기존 인스턴스 반환
+      }
     }
+  }
 }
