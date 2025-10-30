e5ef1e64b4662b69f38f7e15a99a1e256260d483
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-10-30 20:32:18 +0900

    hotfix: Swagger 문서 수정

--- server/src/main/java/com/moup/server/model/dto/WorkCreateResponse.java
+++ server/src/main/java/com/moup/server/model/dto/WorkCreateResponse.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Builder
 @Schema(description = "근무 생성 응답 DTO")
 public class WorkCreateResponse {
-    @Schema(description = "생성된 근무 ID 배열", example = "[\"1\"]", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "생성된 근무 ID 배열", example = "[1]", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<Long> workId;
 }
