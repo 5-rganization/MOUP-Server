5c7ef6cb3ff01c8ae7ec641c84c35d4a7593654a
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-11-03 07:12:35 +0900

    chore: 기존 코드 주석처리
    GitHub Issue: #111

--- server/src/main/java/com/moup/server/config/SwaggerConfig.java
+++ server/src/main/java/com/moup/server/config/SwaggerConfig.java
@@ -22,6 +22,74 @@ import org.springframework.context.annotation.Configuration;
         version = "v1.0.5"))
 public class SwaggerConfig {
 
+//    @Bean
+//    GroupedOpenApi authOpenApi() {
+//        String[] paths = {"/auth/**"};
+//        return GroupedOpenApi.builder().group("인증 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi userOpenApi() {
+//        String[] paths = {"/users/**"};
+//        return GroupedOpenApi.builder().group("유저 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi fileOpenApi() {
+//        String[] paths = {"/files/**"};
+//        return GroupedOpenApi.builder().group("파일 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi adminOpenApi() {
+//        String[] paths = {"/admin/**"};
+//        return GroupedOpenApi.builder().group("관리자 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi workplaceOpenApi() {
+//        String[] pathsToMatch = {"/workplaces/**"};
+//        String[] pathsToExclude = {"/workplaces/**/workers/**", "/workplaces/**/works/**"};
+//        return GroupedOpenApi.builder().group("근무지 관련 API").pathsToMatch(pathsToMatch).pathsToExclude(pathsToExclude).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi workOpenApi() {
+//        String[] pathsToMatch = {"/**/works/**"};
+//        String[] pathsToExclude = {"/**/routines/**"};
+//        return GroupedOpenApi.builder().group("근무 관련 API").pathsToMatch(pathsToMatch).pathsToExclude(pathsToExclude).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi workerOpenApi() {
+//        String[] paths = {"/**/workers/**"};
+//        String[] pathsToExclude = {"/**/works/**"};
+//        return GroupedOpenApi.builder().group("근무자 관련 API").pathsToMatch(paths).pathsToExclude(pathsToExclude).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi routineOpenApi() {
+//        String[] paths = {"/routines/**"};
+//        return GroupedOpenApi.builder().group("루틴 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi alarmOpenApi() {
+//        String[] paths = {"/alarms/**"};
+//        return GroupedOpenApi.builder().group("알림 관련 API").pathsToMatch(paths).build();
+//    }
+//
+//    @Bean
+//    GroupedOpenApi homeOpenApi() {
+//        String[] paths = {"/home/**"};
+//        return GroupedOpenApi.builder().group("홈 화면 관련 API").pathsToMatch(paths).build();
+//    }
+
+    // =================================================================
+    // 디버그용 토큰 표시
+    // - 배포시 삭제 or 주석처리 필요
+    // =================================================================
+
     @Bean
     GroupedOpenApi authOpenApi(OpenApiCustomizer debugTokenCustomizer) {
         return GroupedOpenApi.builder()
@@ -115,18 +183,12 @@ public class SwaggerConfig {
                 .build();
     }
 
-
-    // =================================================================
-    // 디버그용 토큰 표시 메서드
-    // =================================================================
-
-
     /// DebugTokenHolder를 Spring Bean으로 직접 등록합니다.
     /// - **배포시 보여주지 않도록 처리해야 합니다.**
     /// `@Component` 어노테이션을 대체합니다.
     @Bean
     DebugTokenHolder debugTokenHolder() {
-        // TODO: 배포시 보여주지 않도록 처리 (@Profile("!prod") 추가)
+        // TODO: 배포시 보여주지 않도록 처리 (@Profile("!prod") 추가) or 주석처리
         return new DebugTokenHolder();
     }
 
