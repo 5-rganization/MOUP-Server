78cc76bf9ed2e88bde13bc33913ffec5662849c3
neoskyclad <neoskyclad@naver.com>
2025-11-24 21:46:53 +0900

    fix: CORS 설정

--- server/src/main/java/com/moup/server/config/SecurityConfig.java
+++ server/src/main/java/com/moup/server/config/SecurityConfig.java
@@ -19,33 +19,36 @@ import java.util.List;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final String[] NO_AUTH_URL = {"/auth/**"};
-    private final String[] USER_AUTH_URL = {"/users/**", "/files/**", "/workplaces/**", "/routines/**", "/alarms/**"};
-    private final String[] ADMIN_AUTH_URL = {"/admin/**"};
-    private final String[] SWAGGER_URL = {"/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**",
-            "/swagger-ui.html"};
+  private final String[] NO_AUTH_URL = {"/auth/**"};
+  private final String[] USER_AUTH_URL = {"/users/**", "/files/**", "/workplaces/**",
+      "/routines/**", "/alarms/**"};
+  private final String[] ADMIN_AUTH_URL = {"/admin/**"};
+  private final String[] SWAGGER_URL = {"/v3/api-docs/**", "/swagger-ui/**",
+      "/swagger-resources/**",
+      "/swagger-ui.html"};
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(request -> {
-                    var corsConfig = new CorsConfiguration();
-                    corsConfig.setAllowedOrigins(List.of("*"));  // TODO: 프론트엔드 도메인으로 바꾸기
-                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    corsConfig.setAllowedHeaders(List.of("*"));
-                    corsConfig.setAllowCredentials(true);
-                    return corsConfig;
-                })).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-                        auth -> auth.requestMatchers(NO_AUTH_URL).permitAll().requestMatchers(USER_AUTH_URL)
-                                .hasAnyRole("WORKER", "OWNER", "ADMIN").requestMatchers(SWAGGER_URL)
-                                .permitAll()    // TODO: 나중에 swagger 비활성화 하기
-                                .requestMatchers(ADMIN_AUTH_URL).hasRole("ADMIN").anyRequest().authenticated())
-                .exceptionHandling(ex -> ex.authenticationEntryPoint((req, res, e) -> {
-                    res.setStatus(401);
-                }).accessDeniedHandler((req, res, e) -> {
-                    res.setStatus(403);
-                }));
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter)
+      throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(request -> {
+          var corsConfig = new CorsConfiguration();
+          corsConfig.setAllowedOriginPatterns(List.of("*"));
+          corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+          corsConfig.setAllowedHeaders(List.of("*"));
+          corsConfig.setAllowCredentials(true);
+          return corsConfig;
+        })).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        .formLogin(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+            auth -> auth.requestMatchers(NO_AUTH_URL).permitAll().requestMatchers(USER_AUTH_URL)
+                .hasAnyRole("WORKER", "OWNER", "ADMIN").requestMatchers(SWAGGER_URL)
+                .permitAll()    // TODO: 나중에 swagger 비활성화 하기
+                .requestMatchers(ADMIN_AUTH_URL).hasRole("ADMIN").anyRequest().authenticated())
+        .exceptionHandling(ex -> ex.authenticationEntryPoint((req, res, e) -> {
+          res.setStatus(401);
+        }).accessDeniedHandler((req, res, e) -> {
+          res.setStatus(403);
+        }));
+    return http.build();
+  }
 }
