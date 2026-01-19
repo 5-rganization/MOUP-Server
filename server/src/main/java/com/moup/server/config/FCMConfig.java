613ce3442170c8043c6298bbf9a26d3bbb8281fe
neoskyclad <neoskyclad@naver.com>
2026-01-19 20:00:27 +0900

    fix: 오타 수정

--- server/src/main/java/com/moup/server/config/FCMConfig.java
+++ server/src/main/java/com/moup/server/config/FCMConfig.java
@@ -9,10 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 @RequiredArgsConstructor
